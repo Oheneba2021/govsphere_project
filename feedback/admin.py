82765738f04f46a91949c119587101aa0204a135
Godwin Boakye <ohenebayb@gmail.com>
2026-02-11 23:07:27 +0000

    models created for projects,accounts, and feedback. Views added for project list and default dashboard. Static files worked on.

--- feedback/admin.py
+++ feedback/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Feedback
 # Register your models here.
 
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("id",  "show_user", "show_project", "message", "created_at",)
+    list_filter = ("created_at",)
+    search_fields = ("show_project", "message")
+    
+    def show_user(self, obj):
+        if obj.user is None:
+            return "Anonymous"
+        return f"{obj.user.username} "
+    show_user.short_description = "User"
+    
+    def show_project(self, obj):
+        if obj.project is None:
+            return "No Project"
+        return f"{obj.project.title} "
+    show_project.short_description = "Project"
+
+admin.site.register(Feedback, FeedbackAdmin)
--- projects/admin.py
+++ projects/admin.py
@@ -3,4 +3,15 @@ from .models import Project
 # Register your models here.
 
 # Register your models here.
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "budget","location", "status","description", "created_at","show_creator",)
+    list_filter = ("created_at", "status",)
+    search_fields = ("title", "location")
+
+    def show_creator(self, obj):
+        if obj.created_by is None:
+            return "Unknown"
+        return f"{obj.created_by.username}"
+    show_creator.short_description = "Created By"
+admin.site.register(Project, ProjectAdmin)
